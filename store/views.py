07da36c9b86b02542dcec372fe3b6c6736d382da
Shahmir Khan Jadoon <sjaoon@ssidecisions.com>
2021-10-27 07:50:31 +0500

    Replace functional views with ClassBased views

--- store/views.py
+++ store/views.py
@@ -4,22 +4,21 @@ from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.views import APIView
 
 from .models import Product, Collection
 from .serializers import ProductSerializer, CollectionSerializer
 from store import serializers
 
 
-@api_view(['GET', 'POST'])
-def product_list(request):
-
-    if request.method == 'GET':
+class ProductList(APIView):
+    def get(self, request):
         products = Product.objects.select_related('collection').all()
         serializer = ProductSerializer(
             products, many=True, context={'request': request})
         return Response(serializer.data)
 
-    elif request.method == 'POST':
+    def post(self, request):
         # Deserialze incoming ovject
         serializer = ProductSerializer(data=request.data)
         # Validating request below
@@ -29,20 +28,21 @@ def product_list(request):
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
-@api_view(('GET', 'PUT', 'DELETE'))
-def product_detail(request, id):
-    product = get_object_or_404(Product, pk=id)
-    if request.method == 'GET':
+class ProductDetail(APIView):
+    def get(self, request, id):
+        product = get_object_or_404(Product, pk=id)
         serializer = ProductSerializer(product)
         return Response(serializer.data)
 
-    elif request.method == 'PUT':
+    def put(self, request, id):
+        product = get_object_or_404(Product, pk=id)
         serializer = ProductSerializer(product, data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data)
 
-    elif request.method == 'DELETE':
+    def delete(self, request, id):
+        product = get_object_or_404(Product, pk=id)
         if product.orderitems.count() > 0:
             return Response({
                 'error': 'Product cannot be deleted because it is associated with an order item'
@@ -50,8 +50,6 @@ def product_detail(request, id):
 
         product.delete()
         return Response('Product deleted', status=status.HTTP_204_NO_CONTENT)
-
-    # TODO: Give better 404 response
 
 
 #################################################
