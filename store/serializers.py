c888ac026566d3398a0c1df2be222a495036722e
Shahmir Khan Jadoon <sjaoon@ssidecisions.com>
2021-10-30 10:34:10 +0500

    Added JWT token. Login, registration and refresh token implemented

--- store/serializers.py
+++ store/serializers.py
@@ -1,5 +1,5 @@
 from decimal import Decimal
-from store.models import Cart, CartItem, Product, Collection, Review
+from store.models import Cart, CartItem, Customer, Product, Collection, Review
 from rest_framework import serializers
 
 
@@ -70,7 +70,8 @@ class AddCartItemSerializer(serializers.ModelSerializer):
 
     def validate_product_id(self, value):
         if not Product.objects.filter(pk=value).exists():
-            raise serializers.ValidationError('No product with the given ID was found.')
+            raise serializers.ValidationError(
+                'No product with the given ID was found.')
         return value
 
     def save(self, **kwargs):
@@ -78,14 +79,16 @@ class AddCartItemSerializer(serializers.ModelSerializer):
         product_id = self.validated_data['product_id']
         quantity = self.validated_data['quantity']
 
-        try: 
-            cart_item = CartItem.objects.get(cart_id=cart_id, product_id=product_id)
+        try:
+            cart_item = CartItem.objects.get(
+                cart_id=cart_id, product_id=product_id)
             cart_item.quantity += quantity
             cart_item.save()
             self.instance = cart_item
         except CartItem.DoesNotExist:
-            self.instance = CartItem.objects.create(cart_id=cart_id, **self.validated_data)
-        
+            self.instance = CartItem.objects.create(
+                cart_id=cart_id, **self.validated_data)
+
         return self.instance
 
     class Meta:
@@ -97,3 +100,12 @@ class UpdateCartItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = CartItem
         fields = ['quantity']
+
+
+class CustomerSerializer(serializers.ModelSerializer):
+
+    user_id = serializers.IntegerField()
+
+    class Meta:
+        model = Customer
+        fields = ['id', 'user_id', 'phone', 'birth_date', 'membership']
