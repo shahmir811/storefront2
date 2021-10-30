cee3f096715a025a561eddacd8559e91f8305f19
Shahmir Khan Jadoon <sjaoon@ssidecisions.com>
2021-10-30 09:01:39 +0500

    Add user, groups and custom permission

--- store/serializers.py
+++ store/serializers.py
@@ -1,13 +1,9 @@
 from decimal import Decimal
-from logging import raiseExceptions
+from store.models import Cart, CartItem, Product, Collection, Review
 from rest_framework import serializers
 
 
-from .models import Product, Collection, Review, Cart, CartItem
-
-
 class CollectionSerializer(serializers.ModelSerializer):
-
     class Meta:
         model = Collection
         fields = ['id', 'title', 'products_count']
@@ -16,36 +12,22 @@ class CollectionSerializer(serializers.ModelSerializer):
 
 
 class ProductSerializer(serializers.ModelSerializer):
-
     class Meta:
         model = Product
-        fields = ['id', 'title', 'slug', 'inventory',
-                  'unit_price', 'description', 'price_with_tax', 'collection']
-        extra_kwargs = {
-            "title": {
-                "error_messages": {
-                    "required": "Product title is required"
-                }
-            },
-            "unit_price": {
-                "error_messages": {
-                    "required": "Mention product unit price"
-                }
-            }
-        }
+        fields = ['id', 'title', 'description', 'slug', 'inventory',
+                  'unit_price', 'price_with_tax', 'collection']
 
     price_with_tax = serializers.SerializerMethodField(
         method_name='calculate_tax')
 
     def calculate_tax(self, product: Product):
-        return Decimal(format(product.unit_price * Decimal(1.1), '0.2f'))
+        return product.unit_price * Decimal(1.1)
 
 
 class ReviewSerializer(serializers.ModelSerializer):
-
     class Meta:
         model = Review
-        fields = ['id', 'name', 'description', 'date']
+        fields = ['id', 'date', 'name', 'description']
 
     def create(self, validated_data):
         product_id = self.context['product_id']
@@ -59,7 +41,6 @@ class SimpleProductSerializer(serializers.ModelSerializer):
 
 
 class CartItemSerializer(serializers.ModelSerializer):
-
     product = SimpleProductSerializer()
     total_price = serializers.SerializerMethodField()
 
@@ -76,12 +57,8 @@ class CartSerializer(serializers.ModelSerializer):
     items = CartItemSerializer(many=True, read_only=True)
     total_price = serializers.SerializerMethodField()
 
-    def get_total_price(self, cart: Cart):
-        total = 0
-        for item in cart.items.all():
-            total += item.quantity * item.product.unit_price
-
-        return total
+    def get_total_price(self, cart):
+        return sum([item.quantity * item.product.unit_price for item in cart.items.all()])
 
     class Meta:
         model = Cart
@@ -89,14 +66,11 @@ class CartSerializer(serializers.ModelSerializer):
 
 
 class AddCartItemSerializer(serializers.ModelSerializer):
-
     product_id = serializers.IntegerField()
 
-    # Validating product_id
     def validate_product_id(self, value):
         if not Product.objects.filter(pk=value).exists():
-            raise serializers.ValidationError(
-                'Product with given id not found')
+            raise serializers.ValidationError('No product with the given ID was found.')
         return value
 
     def save(self, **kwargs):
@@ -104,20 +78,14 @@ class AddCartItemSerializer(serializers.ModelSerializer):
         product_id = self.validated_data['product_id']
         quantity = self.validated_data['quantity']
 
-        try:
-            # Updating an existing item
-            cart_item = CartItem.objects.get(
-                cart_id=cart_id, product_id=product_id)
+        try: 
+            cart_item = CartItem.objects.get(cart_id=cart_id, product_id=product_id)
             cart_item.quantity += quantity
             cart_item.save()
             self.instance = cart_item
-
         except CartItem.DoesNotExist:
-            # Adding a new item
-            cart_item = CartItem.objects.create(
-                cart_id=cart_id, **self.validated_data)
-            self.instance = cart_item
-
+            self.instance = CartItem.objects.create(cart_id=cart_id, **self.validated_data)
+        
         return self.instance
 
     class Meta:
@@ -126,7 +94,6 @@ class AddCartItemSerializer(serializers.ModelSerializer):
 
 
 class UpdateCartItemSerializer(serializers.ModelSerializer):
-
     class Meta:
         model = CartItem
         fields = ['quantity']
--- store/urls.py
+++ store/urls.py
@@ -1,9 +1,6 @@
 from django.urls import path
 from django.urls.conf import include
 from rest_framework_nested import routers
-
-# from rest_framework import urlpatterns
-
 from . import views
 
 router = routers.DefaultRouter()
@@ -19,4 +16,5 @@ products_router.register('reviews', views.ReviewViewSet,
 carts_router = routers.NestedDefaultRouter(router, 'carts', lookup='cart')
 carts_router.register('items', views.CartItemViewSet, basename='cart-items')
 
+# URLConf
 urlpatterns = router.urls + products_router.urls + carts_router.urls
